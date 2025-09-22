47be0bac4c775c6e5ce27b43874d04df1da53c3b
mlsiebold <m.siebold@live.com>
2025-09-21 21:18:36 -0600

    Update 8

--- asn1_function_sheet.py
+++ asn1_function_sheet.py
@@ -22,9 +22,6 @@ def age_splitter(df, col_name, age_threshold):
     df_above_equal = df[df[col_name] >= age_threshold]
     return df_below, df_above_equal                      
 
-
-
-
     
 def effectSizer(df, num_col, cat_col):
     """
